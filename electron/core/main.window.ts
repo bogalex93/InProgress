ee0885687fd29f726c2389a0865f632893fd00f6
bogalex93 <bog.alex93@gmail.com>
2024-03-04 21:57:28 +0200

    update pkg

--- electron/core/main.window.ts
+++ electron/core/main.window.ts
@@ -7,7 +7,6 @@ import { Crossover } from '../../crossover/crossover-ipc.main';
 import { ConfigurationChannel, DataChannel } from '../../crossover/crossover.channels';
 import { GenericData, GraphicProperties, ReadData } from '../../crossover/crossover.models';
 import { environment } from '../environment';
-//const glasstron = require('glasstron');
 
 export class MainWindow {
   public win: BrowserWindow;
@@ -27,13 +26,12 @@ export class MainWindow {
       skipTaskbar: false,
       webPreferences: webPreferences,
       minimizable: true,
+      titleBarStyle: "hidden",
+      //backgroundMaterial: "acrylic",
       icon: iconPath,
       ...this.graphicProperties.appBounds
     };
     this.win = new BrowserWindow(windowOptions);
-    // (<any>this.win).blurType = "blurbehind";
-    // (<any>this.win).setBlur(true);
-    this.win.setVibrancy('fullscreen-ui');
     this.win.loadURL(appUrl);
     this.win.show();
     //win.webContents.openDevTools();
